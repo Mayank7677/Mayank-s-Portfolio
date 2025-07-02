109a4d54c9c6b146941189b1e989f28635419013
Mayank Panwar <mayankpanwar066@gmail.com>
2025-07-02 13:20:26 +0530

    projects added

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import hms1 from "../assets/hms-1.png";
 import chat1 from "../assets/chat2.png";
 import img1 from "../assets/img2.png";
+import errik1 from "../assets/errik-1.png";
+import wt1 from "../assets/wt-1.png";
 
 const Playground = () => {
   return (
@@ -12,7 +14,7 @@ const Playground = () => {
         each project shaped by learning and code.
       </p>
 
-      <div className="mt-30 md:mt-50 flex flex-col gap-30 md:gap-50">
+      <div className="mt-30 flex flex-col gap-30 md:gap-50">
         <a href="https://hotelmanagement-bxyq.onrender.com">
           <div>
             <div>
@@ -100,6 +102,76 @@ const Playground = () => {
                 enhance images instantly. With Razorpay integration for payments
                 and secure auth, it's perfect for creators needing quick,
                 high-quality image edits.
+              </p>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://erikk-clone.onrender.com">
+          <div>
+            <div>
+              <img
+                className="min-h-50 object-cover rounded-4xl md:rounded-[50px]"
+                src={errik1}
+                alt=""
+              />
+            </div>
+
+            <div>
+              <div className="flex gap-3 md:gap-8 items-center">
+                <p className="text-4xl sm:text-5xl md:text-6xl">
+                  Errik UI Clone
+                </p>
+                <img
+                  className="h-10 md:h-20 rotate-230"
+                  src="https://www.ngan-nguyen.com/images/Visual-DownArrow.svg"
+                  alt=""
+                />
+              </div>
+
+              <p className="mt-3 text-neutral-400 sm:text-lg">
+                One of my best UI design projects. Although it’s a static site,
+                it’s fully responsive and built with clean, modular code. I used
+                React with Tailwind CSS and rendered components dynamically.
+                Framer Motion was used for animations, and I also added smooth
+                scroll effects to enhance the user experience. The entire design
+                focused on clean structure, responsiveness, and interactive
+                feel.
+              </p>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://westside-clone.onrender.com/">
+          <div>
+            <div>
+              <img
+                className="min-h-50 object-cover rounded-4xl md:rounded-[50px]"
+                src={wt1}
+                alt=""
+              />
+            </div>
+
+            <div>
+              <div className="flex gap-3 md:gap-8 items-center">
+                <p className="text-4xl sm:text-5xl md:text-6xl">
+                  Westside Clone
+                </p>
+                <img
+                  className="h-10 md:h-20 rotate-230"
+                  src="https://www.ngan-nguyen.com/images/Visual-DownArrow.svg"
+                  alt=""
+                />
+              </div>
+
+              <p className="mt-3 text-neutral-400 sm:text-lg">
+                A fully responsive and interactive static website inspired by
+                the Westside fashion brand. Built with React and styled using
+                Tailwind CSS, it delivers a rich UI experience with clean layout
+                and smooth navigation. Utilized the Context API for state
+                management to handle features like product filters and UI
+                interactions. Focused on pixel-perfect design and performance
+                across all device sizes.
               </p>
             </div>
           </div>
